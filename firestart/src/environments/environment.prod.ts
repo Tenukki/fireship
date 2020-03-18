591d25c7a3b0fa7ec7452845cec842650a398d94
Tenukki <tenhunens@hotmail.com>
2020-03-18 18:30:10 +0200

    asds

---
+++ firestart/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC2X-rdAIYsrUh9qoPSeKs4kvN1V-NBOBQ",
+    authDomain: "fire-ship.firebaseapp.com",
+    databaseURL: "https://fire-ship.firebaseio.com",
+    projectId: "fire-ship",
+    storageBucket: "fire-ship.appspot.com",
+    messagingSenderId: "132771163399",
+    appId: "1:132771163399:web:5187598235b2398808a9a2"
+  }
+};
