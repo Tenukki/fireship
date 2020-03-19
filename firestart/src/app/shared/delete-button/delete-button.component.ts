246ecf2bcc39f8b7af47035c41e7f7e512ddb57a
Tenukki <tenhunens@hotmail.com>
2020-03-19 14:40:47 +0200

    asds

---
+++ firestart/src/app/shared/delete-button/delete-button.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit,EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-button',
+  templateUrl: './delete-button.component.html',
+  styleUrls: ['./delete-button.component.scss']
+})
+export class DeleteButtonComponent implements OnInit {
+  canDelete: boolean;
+
+  @Output() delete = new EventEmitter<boolean>()
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  prepareForDelete() {
+    this.canDelete = true;
+  }
+
+  cancel() {
+    this.canDelete = false;
+  }
+
+  deleteBoard() {
+    this.delete.emit(true);
+    this.canDelete = false;
+  }
+
+}
